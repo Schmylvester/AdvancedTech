0efa2f9ccdb562a9d7aca2d52844479f3bd56373
Thomas <tomsylvester1@yahoo.co.uk>
2019-04-13 18:35:22 +0100

    Once the finding of the mid point is good, this will be absolutely class

--- collisions/Player.cpp
+++ collisions/Player.cpp
@@ -44,5 +44,5 @@ void Player::update(float dt)
 void Player::collision(CollisionData col, CollisionClassifier type)
 {
 	if(type == CollisionClassifier::Collision_This_Frame)
-		m_physics->addForceAtPoint(18, col.colision_center, Vector3(0, 1, 0));
+		m_physics->addForceAtPoint(0.3f, col.colision_center, m_transform.getPos() - col.colision_center);
 }
